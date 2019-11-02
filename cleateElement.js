0134fa883c81f3757432d7d67b73c7d703948a53
yasutomo90 <yasutomo90@gmail.com>
2019-11-02 23:29:22 +0900

    add

---
+++ cleateElement.js
@@ -0,0 +1,10 @@
+var radioName, radioId, radioValue;
+function createRadio() {
+  var input = document.createElement("input");
+  input.id = radioId;
+  input.value = radioValue;
+  input.type = radio;
+  var label = document.createElement("label");
+  label.for = raidoId;
+  var radioBotan = input + label;
+}

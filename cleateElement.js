85f2cb6fa492ac52ce8e5c089ef56afddeb99cac
yasutomo90 <yasutomo90@gmail.com>
2019-11-02 23:41:19 +0900

    create radioBotan automation

--- cleateElement.js
+++ cleateElement.js
@@ -1,4 +1,5 @@
 var input, label, radioBotan, radioName, radioId, radioValue, radioTitle;
+var targetId = document.getElementById("target");
 function createRadio() {
   input = document.createElement("input");
   input.id = radioId;
@@ -8,4 +9,5 @@ function createRadio() {
   label.for = raidoId;
   label.innerText = radioTitle;
   radioBotan = input + label;
+  targetId.appendChild(radioBotan);
 }

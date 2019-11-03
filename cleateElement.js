cf696134b7030e1ac728dafe2a1ea7a97c6d436e
yasutomo90 <yasutomo90@gmail.com>
2019-11-03 21:58:11 +0900

    create radio complete

--- cleateElement.js
+++ cleateElement.js
@@ -1,15 +1,15 @@
-var radioBotan, radioName, radioId, radioValue, radioTitle;
-
-function createRadio(radioName) {
+function createRadio(targetId, radioName, radioId, radioValue) {
   var input = document.createElement("input");
-  //input.id = radioId;
-  input.value = testValue;
-  input.type = "button";
-  input.name = radioName;
-  //  var label = document.createElement("label");
-  //  label.for = raidoId;
-  //  label.innerText = radioTitle;
-  //  radioBotan = input + label;
-  var targetId = document.getElementById("target");
-  targetId.appendChild(input);
+  input.setAttribute("type", "radio");
+  input.setAttribute("name", radioName);
+  input.setAttribute("id", radioId);
+  input.setAttribute("value", radioValue);
+  var label = document.createElement("label");
+  label.setAttribute("name", radioName);
+  label.setAttribute("for", radioName);
+  label.innerHTML = radioValue;
+  var target = document.getElementById(targetId);
+  target.appendChild(input);
+  target.appendChild(label);
+  target.appendChild("<br>");
 }

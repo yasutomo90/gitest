f4a14c702d4419cfc73e6bb8043d2f76f94dc402
yasutomo90 <yasutomo90@gmail.com>
2019-11-03 21:35:44 +0900

    add test

--- cleateElement.js
+++ cleateElement.js
@@ -1,13 +1,15 @@
-var input, label, radioBotan, radioName, radioId, radioValue, radioTitle;
-var targetId = document.getElementById("target");
-function createRadio() {
-  input = document.createElement("input");
-  input.id = radioId;
-  input.value = radioValue;
-  input.type = radio;
-  label = document.createElement("label");
-  label.for = raidoId;
-  label.innerText = radioTitle;
-  radioBotan = input + label;
-  targetId.appendChild(radioBotan);
+var radioBotan, radioName, radioId, radioValue, radioTitle;
+
+function createRadio(radioName) {
+  var input = document.createElement("input");
+  //input.id = radioId;
+  input.value = testValue;
+  input.type = "button";
+  input.name = radioName;
+  //  var label = document.createElement("label");
+  //  label.for = raidoId;
+  //  label.innerText = radioTitle;
+  //  radioBotan = input + label;
+  var targetId = document.getElementById("target");
+  targetId.appendChild(input);
 }

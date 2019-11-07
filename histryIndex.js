51b17c9f3f61c59dc1800af89b86aa288c1753d3
yasutomo90 <yasutomo90@gmail.com>
2019-11-07 23:12:20 +0900

    add

--- histryIndex.js
+++ histryIndex.js
@@ -1,3 +1,24 @@
 function createHistory() {
-  var histry = [];
+  switch (flag) {
+    case "1":
+      console.log("1だよ")
+      break;
+    case "2":
+      console.log("2だよ")
+      break;
+    case "3":
+      console.log("3だよ")
+      break;
+    case "4":
+      console.log("4だよ")
+      break;
+    case "5":
+      console.log("5だよ")
+      break;
+    case "6":
+      console.log("6だよ")
+      break;
+    default:
+      console.log("違うよ")
+  }
 }

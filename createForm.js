610eb7386d14e5f07953ec509555b8a3e6864a8b
yasutomo90 <yasutomo90@gmail.com>
2019-11-07 13:03:25 +0900

    ad

--- createForm.js
+++ createForm.js
@@ -3,28 +3,20 @@ var flag = 0;
 
 document.addEventListener("DOMContentLoaded", function () {
   var rent = document.getElementById("btn1");
-  rent.addEventListener("click", radio1());
+  rent.addEventListener("click", radio1);
 }, false);
 
 function radio1() {
   var target = document.getElementById("target");
   target.innerHTML = "";
-
   createBtn("radio", "payment", "pay1", "phone");
   createBtn("radio", "payment", "pay2", "card");
   createBtn("radio", "payment", "pay3", "bank");
-
   createBr("target");
-
   createBtn("radio", "after", "afterY", "yes");
   createBtn("radio", "after", "afterN", "no");
-
   createBr("target");
-
   createBtn("checkbox", "send", "send", "send");
-
   createBr("target");
-
   createBtn("checkbox", "caution", "caution", "caution");
-
 }

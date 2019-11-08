877fd061489194d2e698a1171c17b54b0d6dc4d0
yasutomo90 <yasutomo90@gmail.com>
2019-11-08 23:05:13 +0900

    add

---
+++ getElement.js
@@ -0,0 +1,49 @@
+function selectTime() {
+    var timeSelect = document.createElement("select");
+    timeSelect.setAttribute("id", "timeSelect");
+    timeSelect.setAttribute("name", "timeSelect");
+    var target = document.getElementById("target");
+    target.appendChild(timeSelect);
+    tango = document.getElementById("timeSelect");
+    for (i = 9; i < 22; i++) {
+        var tomeOption = document.createElement("option");
+        var k = ("0" + i).slice(-2);
+        var x = k + "：00";
+        tomeOption.setAttribute("value", x);
+        tomeOption.innerHTML = x;
+        tango.appendChild(tomeOption);
+    }
+}
+
+function getdates() {
+    var date = document.getElementById("cal").value;
+    var tango = document.getElementById("target");
+    tango.innerText = date;
+}
+
+function getRadioValue(name, j) {
+    var radios = document.getElementsByName(name);
+    for (var i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            hLogs[j] = radios[i].value;
+            break;
+        } else {
+            hLogs[j] = "";
+        }
+    }
+}
+
+function getTxt(target, j) {
+    var tango = document.getElementById(target);
+    hLogs[j] = tango.value;
+}
+
+function histryChk() {
+    var j = 0;
+    for (var i = 0; i < hLogs.length; i++) {
+        if (hLogs[i] != "") {
+            hLogEx[j] = hLogs[i];
+            j++;
+        }
+    }
+}

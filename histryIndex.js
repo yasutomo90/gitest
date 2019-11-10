9c712bfd6eea4de678f0e47750f24293ec6f97dc
yasutomo90 <yasutomo90@gmail.com>
2019-11-10 23:29:48 +0900

    add

--- histryIndex.js
+++ histryIndex.js
@@ -1,12 +1,7 @@
 function createHistory() {
   switch (flag) {
     case 1:
-      getTxt("memoBox", 0);
-      getRadioValue("payment", 1);
-      getRadioValue("after", 2);
-      getRadioValue("send", 3);
-      getRadioValue("sending", 4);
-      getRadioValue("caution", 5);
+      historyPatarn1();
       histryChk();
       console.log("1だよ");
       console.log(hLogEx.join("\r\n"));
@@ -14,20 +9,7 @@ function createHistory() {
       break;
 
     case 2:
-      getTxt("memoBox", 0);
-      getTxt("tel", 3);
-      //if(hLogsがからじゃなかったら)"文字列"+hLogs
-      if (hLogs[3] != "") {
-        hLogs[3] = "連絡先：" + hLogs[3];
-      }
-      getTxt("cal", 1);
-      if (document.getElementById("caLabel") != null) {
-        hLogs[1] = "連絡希望日　" + hLogs[1];
-      }
-      getTxt("timeSelect", 2);
-      if (document.getElementById("timeSelect") != null) {
-        hLogs[2] = "連絡希望時間　" + hLogs[2] + "頃";
-      }
+      historyPatarn2();
       histryChk();
       console.log("2だよ");
       console.log(hLogEx.join("\r\n"));
@@ -35,15 +17,8 @@ function createHistory() {
       break;
 
     case 3:
-      getTxt("memoBox", 0);
-      getRadioValue("payment", 4);
-      getRadioValue("after", 5);
-      getRadioValue("send", 6);
-      getRadioValue("sending", 7);
-      getRadioValue("caution", 8);
-      getTxt("cal", 1);
-      getTxt("timeSelect", 2);
-      getTxt("tel", 3);
+      historyPatarn1();
+      historyPatarn2();
       histryChk();
       console.log("3だよ");
       console.log(hLogEx.join("\r\n"));
@@ -78,5 +53,34 @@ function createHistory() {
 
     default:
       console.log("違うよ");
+  }
+}
+
+function historyPatarn1() {
+  getTxt("memoBox", 0);
+  getRadioValue("payment", 4);
+  getRadioValue("after", 5);
+  getRadioValue("send", 6);
+  getRadioValue("sending", 7);
+  getRadioValue("caution", 8);
+}
+
+function historyPatarn2() {
+  getTxt("memoBox", 0);
+  getTxt("tel", 1);
+  if (hLogs[1] != "") {
+    hLogs[1] = "連絡先：" + hLogs[1];
+  }
+  getTxt("cal", 2);
+  if (document.getElementById("caLabel") != null) {
+    hLogs[2] = "連絡希望日　" + hLogs[2];
+  } else {
+    hLogs[2] = "連絡希望日時　なし";
+  }
+  getTxt("timeSelect", 3);
+  if (document.getElementById("timeSelect") != null) {
+    hLogs[3] = "連絡希望時間　" + hLogs[3] + "頃";
+  } else {
+    hLogs[3] = "";
   }
 }

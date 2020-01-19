3c26378960bb2f5ecf7c38da7a415ec952f8a5a3
yasutomo90 <yasutomo90@gmail.com>
2020-01-19 16:28:56 +0900

    add

--- src/index.js
+++
@@ -1,7 +0,0 @@
-window.onload = function inbox() {
-    create_ul("target", "test_ul_id", "test_ul_class");
-    //          (target_id,     li_id,         li_class, a_blank, a_link, a_text)
-    //create_li_a("test_ul_id", "test_li_id", "test_li_class", "y", "#", "test_text");
-    create_li("test_ul_id", "test_li_id", "test_li_class");
-    create_li("test_ul_id", "test_li_id1", "test_li_class");
-}

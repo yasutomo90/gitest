0415edc839329d6bda764fd679ee8e17fc7ebb0b
yasutomo90 <yasutomo90@gmail.com>
2020-02-11 00:05:10 +0900

    add

--- tokka/src/main.js
+++ tokka/src/main.js
@@ -1,9 +1,10 @@
-window.onload = () => {
-  let target_month = document.getElementById("month");
-  for (var i = 1; i <= 12; i++) {
-    let opt = document.createElement("option");
-    opt.setAttribute = ("value", i);
-    opt.innerHTML = i;
-    target_month.appendChild(opt);
-  }
+window.onload = function () {
+  console.log('test');
+  //  let target_month = document.getElementById("month");
+  //  for (var i = 1; i <= 12; i++) {
+  //    let opt = document.createElement("option");
+  //    opt.setAttribute = ("value", i);
+  //    opt.innerHTML = i;
+  //    target_month.appendChild(opt);
+  //  }
 }

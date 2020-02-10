5ea7366c3aa101d1670bad3443b84831d0fe1697
yasutomo90 <yasutomo90@gmail.com>
2020-02-10 23:42:25 +0900

    add

---
+++ tokka/src/main.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+  let target_month = document.getElementById("month");
+  for (var i = 1; i <= 12; i++) {
+    let opt = document.createElement("option");
+    opt.setAttribute = ("value", i);
+    opt.innerHTML = i;
+    target_month.appendChild(opt);
+  }
+}

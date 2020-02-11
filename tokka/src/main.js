8ecd08108a3209ef59107aa744cc6fcc5b058c0e
yasutomo90 <yasutomo90@gmail.com>
2020-02-11 22:53:51 +0900

    add

--- tokka/src/main.js
+++ tokka/src/main.js
@@ -1,10 +1,32 @@
-window.onload = function () {
-  console.log('test');
-  //  let target_month = document.getElementById("month");
-  //  for (var i = 1; i <= 12; i++) {
-  //    let opt = document.createElement("option");
-  //    opt.setAttribute = ("value", i);
-  //    opt.innerHTML = i;
-  //    target_month.appendChild(opt);
-  //  }
+let inputbox_value = ["test1", "test2", "test3", "test4", "test5"];
+
+window.addEventListener("load", function () {
+  set_selectbox();
+  textbox_create();
+}, false)
+
+
+let set_selectbox = () => {
+  let target_month = document.getElementById("month");
+  for (var i = 1; i <= 12; i++) {
+    let opt = document.createElement("option");
+    opt.setAttribute = ("value", i);
+    opt.innerHTML = i;
+    target_month.appendChild(opt);
+  }
+}
+
+let textbox_create = () => {
+  let target = document.getElementById("target_test");
+  let div_test = document.createElement("div");
+  let input = document.createElement("input");
+  let label = document.createElement("label");
+  div_test.setAttribute = ("class", "input_form");
+  input.setAttribute = ("type", "text");
+  input.setAttribute = ("id", "test");
+  label.setAttribute = ("for", "test");
+  label.innerHTML = "tetestetsetse";
+  div_test.appendChild(input);
+  div_test.appendChild(label);
+  target.appendChild(div_test);
 }

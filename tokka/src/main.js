4f7e8329d10562b21722e068d2d052eb48eb18bb
yasutomo90 <yasutomo90@gmail.com>
2020-02-15 12:10:04 +0900

    addd

--- tokka/src/main.js
+++ tokka/src/main.js
@@ -1,44 +1,41 @@
-let inputbox_value = ["test1", "test2", "test3", "test4", "test5"];
-
-window.addEventListener("load", function () {
-  set_selectbox("target_day1");
-  textbox_create();
-}, false)
-
-
-let set_selectbox = target => {
-  let target_month = document.getElementById(target);
-  for (var i = 9; i <= 19; i++) {
-    let opt = document.createElement("option");
-    opt.setAttribute = ("value", i);
-    opt.innerHTML = i;
-    target_month.appendChild(opt);
-  }
-}
-
-let textbox_create = () => {
-  let target = document.getElementById("target_test");
-  let div_test = document.createElement("div");
-  let input = document.createElement("input");
-  let label = document.createElement("label");
-  div_test.setAttribute = ("class", "input_form");
-  input.setAttribute = ("type", "text");
-  input.setAttribute = ("id", "test");
-  label.setAttribute = ("for", "test");
-  label.innerHTML = "tetestetsetse";
-  div_test.appendChild(input);
-  div_test.appendChild(label);
-  target.appendChild(div_test);
-}
-
-
-//参考
-var customers = {
-  "田中太郎": "03-53xx-xxxx",
-  "田中二郎": "03-54xx-xxxx",
-  "田中花子": "03-55xx-xxxx"
+let id_value = {
+  "test1": "testいち",
+  "test2": "testにー",
+  "test3": "testさん",
+  "test4": "testしー",
+  "test5": "testごー",
+  "test6": "testろく",
+  "test7": "testなな",
+  "test8": "testはち",
+  "test9": "testくー"
 };
 
-for (key in customers) {
-  alert(key + "さんの番号は、" + customers[key] + "です。");
+window.addEventListener("load", () => {
+  //
+  create_test_elem();
+}, false);
+
+
+let create_test_elem = () => {
+
+  let target = document.getElementById("left");
+
+  for (key in id_value) {
+    let div = document.createElement("div");
+    let input = document.createElement("input");
+    let label = document.createElement("label");
+
+    div.setAttribute("class", "input_form");
+    input.setAttribute("type", "text");
+    input.setAttribute("id", key);
+    input.setAttribute("value", '');
+    label.setAttribute("for", key);
+    label.innerHTML = id_value[key];
+    div.appendChild(input);
+    div.appendChild(label);
+    target.appendChild(div);
+    div = "";
+    input = "";
+    label = "";
+  }
 }

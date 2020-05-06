cfa5193ab913680690b43f00ec3532ff19d2127a
yasutomo90 <yasutomo90@gmail.com>
2020-05-07 00:06:56 +0900

    ad

--- vue/vue.js
+++ vue/vue.js
@@ -1,19 +1,50 @@
 Vue.component('test-comp', {
-  template: ``
-})
+  template: `<form @input="submit">
+  <input type="text" v-model="userId" placeholder="input userID">
+  <input type="text" v-model="userName" placeholder="input user NAME">
+  </form>`,
+  data: function(){
+    return {
+      userId: null,
+      userName: null
+    };
+  },
+  methods: {
+    submit: function(){
+      this.$emit('update',{
+        userId: this.userId,
+        userName: this.userName
+      })
+    }
+  },
+
+});
 
 var app = new Vue({
   el: '#app',
   data: {
     testname: null,
     csModel: null,
+
+    form:{
+      userId: null,
+      userName: null,
+    },
+
     pcradios: [
       { value: 'pcSupport'},
       { value: 'tktn'}
     ],
+
     csradios:[
       { value: 'giga'},
       { value: 'cl'}
     ]
+  },
+
+  methods:{
+    updateForm:function(formData){
+      Object.assign(this.form, formData)
+    }
   }
 })

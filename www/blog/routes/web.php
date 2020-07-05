6f6c4e0f437bd3bd0cdba562177e79c911795858
yasutomo90 <yasutomo90@gmail.com>
2020-07-05 23:30:45 +0900

    ad

--- www/blog/routes/web.php
+++ www/blog/routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Auth::routes();
+
+Route::get('/', 'PostController@index')->name('posts.index');
+
+Route::resource('/posts', 'PostController',['except' => ['index']]);
